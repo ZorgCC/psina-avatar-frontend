9f75dccfd00bf68427e7ee388f760c8d13b30040
Aleksei Tsybin <atsybin@saber3d.net>
2022-02-25 21:54:32 +0300

    feature: header, login page

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -6,18 +6,59 @@
       </b-navbar-item>
     </template>
     <template #start>
-      <b-navbar-item class="is-link" tag="router-link" :to="{ name: 'home' }">
+      <b-navbar-item
+        class="is-link"
+        exact-active-class="home-active"
+        tag="router-link"
+        :to="{ name: 'home' }"
+        onclick="this.blur()"
+      >
         Home
       </b-navbar-item>
-      <b-navbar-item href="#"> Docs</b-navbar-item>
-      <b-navbar-item href="#"> Source</b-navbar-item>
+      <b-navbar-item
+        href="https://github.com/ZorgCC/psina-avatar-server/wiki"
+        rel="noopener noreferrer"
+        target="_blank"
+        onclick="this.blur()"
+      >
+        Docs
+      </b-navbar-item>
+      <b-navbar-item
+        href="https://github.com/ZorgCC/psina-avatar-server"
+        rel="noopener noreferrer"
+        target="_blank"
+        onclick="this.blur()"
+      >
+        Source</b-navbar-item
+      >
     </template>
 
     <template #end>
       <b-navbar-item tag="div">
         <div class="buttons">
           <router-link
+            class="button is-light has-background-white is-rounded"
+            :to="{ name: 'settings' }"
+          >
+            <!-- TODO create route setting -->
+            <b-icon
+              :class="{ 'home-active': isAuth }"
+              class="settings-hover"
+              icon="cog"
+            ></b-icon>
+          </router-link>
+          <b-button
+            v-if="isAuth"
             class="button is-light is-rounded"
+            active-class="login-active"
+            @click="isAuth = false"
+          >
+            Log out
+          </b-button>
+          <router-link
+            v-else
+            class="button is-light is-rounded"
+            active-class="login-active"
             :to="{ name: 'login' }"
           >
             Log in
@@ -30,6 +71,21 @@
 <script>
 export default {
   name: 'HeaderComponent',
+  data() {
+    return {
+      isAuth: false,
+    }
+  },
 }
 </script>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.home-active {
+  color: orange;
+}
+.login-active {
+  border-color: orange !important;
+}
+.settings-hover:hover {
+  color: orange;
+}
+</style>
